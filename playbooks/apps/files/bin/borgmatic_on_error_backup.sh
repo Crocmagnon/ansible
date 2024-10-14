20a9c38abb4fe7d0131baf1cc51515caa0041f25
Gabriel Augendre <gabriel@augendre.info>
2024-10-14 18:17:37 +0200

    setup rclone sync + update restic excludes

--- playbooks/apps/files/bin/borgmatic_on_error_backup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if docker ps --format "{{.Names}}" | grep "^nextcloud-aio-nextcloud$"; then
-    docker exec -en nextcloud-aio-nextcloud bash /notify.sh "Remote Borg backup FAILED!" "Repository: $1 failed to synchronise with error: $2 when executing command: $3>."
-else
-    echo "Repository: $1 failed to synchronise with error: $2 when executing command: $3"
-fi
